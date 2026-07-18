0ff9a08670de54a22abbd35cac9b7738de6e63b6
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-07-18 09:24:04 +0900

    [add]userモデルルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
-    resources :users, only:[:index, :show]
+    resources :users, only:[:index, :show, :edit, :update]
     resources :comments, only: [:create, :destroy]
     resources :favorites, only:[:create, :destroy]
   end
