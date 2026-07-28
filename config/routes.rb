5397f4c3f3f3f1257172dd9f2265339b168ee3ce
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2026-07-28 22:59:20 +0900

    [add]relationshipsのコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :show, :edit, :update] do
     member do
       get :follows, :followers
     end
